10418a2364d5a182313a86374b6d423e908c6d8e
Arsh069 <mohammad.arsh6666@gmail.com>
2023-05-25 00:29:31 +0530

    updated

--- ToDo.js
+++ ToDo.js
@@ -1,3 +1,4 @@
+var arr=[]
 if(document.readyState=='loading'){
     document.addEventListener('DOMContentLoaded',ready);
 }
@@ -6,27 +7,68 @@ else{
 }
 
 function ready(){
-   
+    
     var textBoxbtn=document.getElementsByClassName('input-text-btn');
-    //console.log(textBoxbtn)
+ 
     for(var i=0;i<textBoxbtn.length;i++){
-        var button=textBoxbtn[i];
+        var button=textBoxbtn[i];       
         button.addEventListener('click',addBtnClicked)
+        
     }
+
+   window.onload=function(){
+        var localTasks=JSON.parse(localStorage.getItem('data'));
+        
+        for(var j=0;j<localTasks.length;j++){         
+            addTaskToList(localTasks[j]);
+        }
+        
+
+   }
+   
+
 
 }
 
+
+
+
 function removeTask(event){
     var butn=event.target
-    console.log(butn.parentElement)
+    
+    var localTasks=JSON.parse(localStorage.getItem('data'));
+    for(var i=0;i<localTasks.length;i++){
+        var localid=butn.parentElement.parentElement;
+        var dummy=localid.getElementsByClassName("item-list")[0].innerHTML
+        if(dummy==localTasks[i]){
+            console.log('true')
+            var index = localTasks.indexOf(dummy);
+            localTasks.splice(index,1)
+            localStorage.setItem('data',JSON.stringify(localTasks))
+        }
+    }
+   
+    
     butn.parentElement.parentElement.remove()
     
 }
 
 
-function addBtnClicked(event){
+function addBtnClicked(){
    var text=document.getElementById("My-id").value
-   addTaskToList(text);
+   if(text==''){
+    alert('Enter the task');
+   }
+
+  else{
+    var text=document.getElementById("My-id").value
+    
+    arr.push(text);
+    localStorage.setItem('data',JSON.stringify(arr))
+    
+    addTaskToList(text);
+    document.getElementById("My-id").value='';
+  }
    
 }
 
@@ -35,12 +77,26 @@ function addTaskToList(text){
     var task=document.createElement('div')
     task.className="item-div"
     var taskItems=document.getElementsByClassName("list-elements")[0]
+    
+    var taskItemsTitle=document.getElementsByClassName("item-list")
+    for(var i=0;i<taskItemsTitle.length;i++){
+        if(text==taskItemsTitle[0].innerHTML){
+            alert("Task Already in the List")
+            return;
+        }
+    }
+
+
     var taskListContent=`
             <li class="item-list">${text}</li> 
             <button class="item-btn header-btn btn" onclick="removeTask(event)" type="button"><img src="images/check.png"></button>  
             `
-    task.innerHTML=taskListContent
-    taskItems.append(task)
-
+    
+    
+           // localStorage.setItem('taskItem',taskListContent);
+            task.innerHTML=taskListContent
+           // var taski=localStorage.getItem('taskItem').value;
+            taskItems.append(task)
+    
 
 }
